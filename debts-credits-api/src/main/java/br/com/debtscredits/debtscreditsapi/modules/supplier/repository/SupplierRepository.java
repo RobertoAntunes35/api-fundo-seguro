7722c4ffa400c63b7fa6b3c77082401db779e39d
RobertoAntunes35 <111751478+RobertoAntunes35@users.noreply.github.com>
2023-11-21 19:52:59 -0300

    Delete debts-credits-api/src/main/java/br/com/debtscredits/debtscreditsapi/modules/supplier directory

--- debts-credits-api/src/main/java/br/com/debtscredits/debtscreditsapi/modules/supplier/repository/SupplierRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.debtscredits.debtscreditsapi.modules.supplier.repository;
-
-import br.com.debtscredits.debtscreditsapi.modules.supplier.model.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
-
-}
